a6971089be3ae0c9e02fd06401a4bbed4cc0521a
Nipun-S-05 <naniwork2022@gmail.com>
2024-08-07 12:13:16 +0530

    Update Traffic_Combinational.v

--- rtl/Traffic_Combinational.v
+++ rtl/Traffic_Combinational.v
@@ -1,21 +1,21 @@
 module Traffic_Combinational(
-    input wire [1:0] i_G, 
-    output reg o_Main_red,
-    output reg o_Main_yellow,
-    output reg o_Main_green,
-    output reg o_Side_red, 
-    output reg o_Side_yellow,
-    output reg o_Side_green,
-    output reg o_Long_trigger,
-    output reg o_Short_trigger
+    input wire [1:0] i_G,     //THe gray code state input
+    output reg o_Main_red,    //Output red light of the main traffic light
+    output reg o_Main_yellow, //Output yellow light of the main traffic light
+    output reg o_Main_green,  //Output green light of the main traffic light
+    output reg o_Side_red,    //Ouput red light of the side traffic light
+    output reg o_Side_yellow, //Output yellow light of the side traffic light
+    output reg o_Side_green,  //Output green light of the side traffic light
+    output reg o_Long_trigger, //Output long trigger 
+    output reg o_Short_trigger //Output short trigger
     );
-    parameter p_S0 = 2'b00, 
+    parameter p_S0 = 2'b00, //The parameter gray code states
               p_S1 = 2'b01, 
               p_S2 = 2'b11, 
               p_S3 = 2'b10;
 
     always @(*) begin
-        case (i_G)
+        case (i_G)   //The case statement is used t mention the behavior of the combinational logic based on the FSM input recieved 
             p_S0: begin
                 o_Main_red <= 0;
                 o_Main_yellow <= 0;
